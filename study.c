7eeebef67e88a54ee584263415f7cae7e2d244a0
coke-jiyong <127110285+coke-jiyong@users.noreply.github.com>
2023-06-30 19:20:25 +0900

    11399 ATM
    11399 ATM

--- study.c
+++ study.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+
 
 int main()
 {
-	int kg;
-	scanf_s("%d", &kg);
-	for (int i = 0; i <= kg / 3; i++)
+	int N;
+	scanf_s("%d", &N);
+	int* P = calloc(N, sizeof(int));
+	int tmp;
+	int min=0;
+	int minResult = 0;
+	int sum = 0;
+	int result = 0;
+	if (P == NULL)	printf("calloc() error!");
+	else 
 	{
-		if ((kg - 3 * i) % 5 == 0) 
+		for (int i = 0; i < N; i++)
 		{
-			printf("%d\n", i + ((kg - 3 * i) / 5));
-			exit(0);
+			scanf_s("%d", &P[i]);
+			min += P[i];
+			minResult += min;
 		}
+		for (int i = 0; i < N - 1; i++)
+		{
+			for (int j = i + 1; j < N; j++)
+			{
+				if (P[i] > P[j])
+				{
+					tmp = P[i];
+					P[i] = P[j];
+					P[j] = tmp;
+				}
+			}
+		}
+		
+		for (int i = 0; i < N; i++)
+		{
+			sum += P[i];
+			result += sum;
+		}
+
+		printf("%d", result > minResult ? minResult : result);
+		
 	}
-	printf("%d", -1);
+	
+	free(P);
 	return 0;
 }
 
